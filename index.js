372ff009af44a6cfbe627d1f048f1ac4f1dc7cf8
Jenny <jenny.kats@aol.com>
2017-01-20 14:05:07 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function receivesAFunction(callback) {
+  callback()
+}
+
+function aThing () {
+  return "hi"
+}
+
+function returnsANamedFunction() {
+  return aThing
+}
+
+
+
+function returnsAnAnonymousFunction() {
+  return function() {
+    console.log("Hi!")
+  }
+}
